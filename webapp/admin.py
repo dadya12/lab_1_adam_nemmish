f713d264a8bf7a4899bcfa5600eceb6bf6b77e6c
Adam <adam.nemmish213@gmail.com>
2023-12-03 19:07:32 +0600

    Создал модели категорий и модели продуктов, Зарегестрировал их в админке. Накатил миграцию и применил ее. Создал шаблон base.html

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from webapp.models import Category, Product
 
 # Register your models here.
+@admin.register(Category)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description']
+    list_filter = ['id', 'name']
+    search_fields = ['id', 'name']
+    fields = ['name', 'description']
+
+@admin.register(Product)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'category', 'date', 'price', 'imagine']
+    list_filter = ['id', 'name']
+    search_fields = ['id', 'name']
+    fields = ['name', 'description', 'category', 'date', 'price', 'imagine']
+    readonly_fields = ['date']
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Наименование')
+    description = models.TextField(max_length=200, verbose_name='Описание', null=True, blank=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(max_length=200, verbose_name='Описание', null=True, blank=True)
+    category = models.ForeignKey('webapp.Category', verbose_name='Категория', on_delete=models.RESTRICT)
+    date = models.DateTimeField(auto_now_add=True, verbose_name='Дата и время добавления')
+    price = models.DecimalField(verbose_name='Стоимость', max_digits=10, decimal_places=0)
+    imagine = models.CharField(verbose_name='Изображение', max_length=300)
